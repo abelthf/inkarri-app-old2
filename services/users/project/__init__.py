55e786a27b455aa77c52000e895da7259334c326
Abel Huanca <abel.huanca@upeu.edu.pe>
2018-09-09 11:34:03 -0500

    reconfigurando compose

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask, jsonify
 
 # instanciamos la app
 app = Flask(__name__)
 
 #configuracion
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
