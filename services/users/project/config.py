6f3b8c3775e69aacc7cae6fea04885ca8fc021de
Abel Huanca <abel.huanca@upeu.edu.pe>
2018-09-09 11:49:42 -0500

    agregando base de datos

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,15 +1,19 @@
+import os
+
 class BaseConfig:
     """Configuracion base del proyecto"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
     """Configuracion de desarrollo"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig(BaseConfig):
     """Configuracion de pruebas"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
     """Configuracion de produccion"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
