0b5a45e75428bfb89a2575f2c7ad00d79a6ace46
Abel Huanca <abel.huanca@upeu.edu.pe>
2018-09-09 09:50:08 -0500

    fin primera parte

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask, jsonify
 # instanciamos la app
 app = Flask(__name__)
 
+#configuracion
+app.config.from_object('project.config.DevelopmentConfig')
+
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
